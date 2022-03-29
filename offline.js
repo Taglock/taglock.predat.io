7a941fb820a44d8b593d56a4fa9fb89d614103b2
Taglock <39584594+Taglock@users.noreply.github.com>
2022-03-29 08:48:37 +0500

    Initial commit

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1648525239,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/rabbyt-sheet0.png",
+		"images/carrot-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/fox-sheet0.png",
+		"images/tiledbackground4.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
